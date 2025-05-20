96fa037e2790e67927aeba9fa7e8ce68cf7a5edd
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-05-20 23:38:36 +0530

    chore: code refactor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,21 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TokenBucketGaurd } from 'src/rate-limiter/token-bucket.gaurd';
+// import { LeakyBucketGuard } from 'src/rate-limiter/leaky-bucket.gaurd';
 // import { FixedWindowCounterGaurd } from 'src/rate-limiter/fixed-window-counter.gaurd';
+// import { SlidingWindowLogGuard } from 'src/rate-limiter/sliding-window-log.gaurd';
+// import { SlidingWindowCounterGuard } from 'src/rate-limiter/sliding-window-counter.gaurd';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @UseGuards(FixedWindowCounterGaurd)
   @UseGuards(TokenBucketGaurd)
+  // @UseGuards(LeakyBucketGuard)
+  // @UseGuards(FixedWindowCounterGaurd)
+  // @UseGuards(SlidingWindowLogGuard)
+  // @UseGuards(SlidingWindowCounterGuard)
   getHello(): string {
     return this.appService.getHello();
   }
